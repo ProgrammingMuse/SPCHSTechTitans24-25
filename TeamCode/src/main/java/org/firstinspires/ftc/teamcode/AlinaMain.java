0b840ad2ccba45ffd6852c6de86082824d7dfaa6
alinaw <wasupanalina@gmail.com>
2024-09-26 14:14:30 -0400

    AlinaMain can contain different functions
    I turned OmniDrive into a function that can be called in AlinaMain so that later, we can add sliding functions into just one main so that robot can use slider and can drive around.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlinaMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlinaMain.java
@@ -1,4 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
-public class AlinaMain {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "AlinaMain")
+public class AlinaMain extends LinearOpMode {
+
+    private ElapsedTime runtime;
+    public AlinaMain() {
+        runtime = new ElapsedTime();
+    }
+
+    @Override
+    public void runOpMode() {
+        // Initialize hardware and omniDriveHelper
+        OmniDrive OmniFunction = new OmniDrive(hardwareMap);
+
+        // Wait for the game to start (driver presses PLAY)
+        telemetry.addData("Initialized","Status");
+        telemetry.update();
+        waitForStart();
+        runtime.reset();
+        // Run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // Call the omniFunction method and pass in the gamepad and telemetry
+            OmniFunction.OmniUpdate(gamepad1,telemetry);
+        }
+    }
 }
